2ed8f2e197bdfa0326f39e7f7326b7bad08d1b53
Chaostastic <roodey72mc@gmail.com>
2022-11-02 10:43:33 +0200

    Reset when game ends, fix and clean code

--- script.js
+++ script.js
@@ -4,21 +4,11 @@ let wins = 0;
 let losses = 0;
 let draws = 0;
 
-function evaluate(playerSelection, computerSelection) {
-    switch (true) {
-        case playerSelection === computerSelection:
-            wins++;
-            return "Draw";
-        case playerSelection === "rock" && computerSelection === "paper":
-        case playerSelection === "paper" && computerSelection === "scissors":
-        case playerSelection === "scissors" && computerSelection === "rock":
-            losses++;
-            return "Computer wins";
-        case playerSelection === "rock" && computerSelection === "scissors":
-        case playerSelection === "paper" && computerSelection === "rock":
-        case playerSelection === "scissors" && computerSelection === "paper":
-            draws++;
-            return "You win";
+function playRound(e) {
+    const playerSelection = this.id;
+    evaluate(playerSelection, getComputerChoice());
+    if (wins >= 5 || losses >= 5) {
+        endGame();
     }
 }
 
@@ -30,6 +20,33 @@ function getComputerChoice() {
     }
 }
 
+function evaluate(playerSelection, computerSelection) {
+    switch (true) {
+        case playerSelection === computerSelection:
+            draws++;
+            console.log("You play " + playerSelection + ", computer plays " + computerSelection + ". Draw");
+            break;
+        case playerSelection === "rock" && computerSelection === "paper":
+        case playerSelection === "paper" && computerSelection === "scissors":
+        case playerSelection === "scissors" && computerSelection === "rock":
+            losses++;
+            console.log("You play " + playerSelection + ", computer plays " + computerSelection + ". Computer wins");
+            break;
+        case playerSelection === "rock" && computerSelection === "scissors":
+        case playerSelection === "paper" && computerSelection === "rock":
+        case playerSelection === "scissors" && computerSelection === "paper":
+            wins++;
+            console.log("You play " + playerSelection + ", computer plays " + computerSelection + ". You win");
+    }
+}
+
+function endGame() {
+    console.log(getGameResult() + " Wins: " + wins + " Losses: " + losses + " Draws: " + draws);
+    wins = 0;
+    losses = 0;
+    draws = 0;
+}
+
 function getGameResult() {
     switch (true) {
         case wins > losses: return "You win!";
@@ -38,19 +55,4 @@ function getGameResult() {
     }
 }
 
-function playRound(e) {
-    const playerSelection = this.id;
-    const computerSelection = getComputerChoice();
-    const result = (evaluate(playerSelection, computerSelection))
-    if (result === undefined) {
-        alert("Invalid input, try again.");
-    }
-    console.log("You play " + playerSelection + ", computer plays " + computerSelection + ". " + result);
-    if (wins >= 5 || losses >= 5) {
-        console.log(getGameResult() + " Wins: " + wins + " Losses: " + losses + " Draws: " + draws);
-    }
-}
-
-buttons.forEach((button => button.addEventListener("click", playRound)))
-
-
+buttons.forEach((button => button.addEventListener("click", playRound)));

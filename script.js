ac9759059eee76426f1eea548197af67ffd0c675
Chaostastic <roodey72mc@gmail.com>
2022-11-02 11:52:29 +0200

    Stop game at end, display game result, add reset button

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-
-const buttons = document.querySelectorAll("button")
+let gameOver = false;
 let wins = 0;
 let losses = 0;
 let draws = 0;
 
+const buttons = document.querySelectorAll(".choice-button");
+buttons.forEach((button => button.addEventListener("click", playRound)));
+
 function playRound(e) {
+    if (gameOver) return;
     const playerSelection = this.id;
     evaluate(playerSelection, getComputerChoice());
     updateCounters();
@@ -21,37 +24,54 @@ function getComputerChoice() {
     }
 }
 
+function evaluate(playerSelection, computerSelection) {
+    switch (true) {
+        case playerSelection === computerSelection:
+            draws++;
+            break;
+        case playerSelection === "rock" && computerSelection === "paper":
+        case playerSelection === "paper" && computerSelection === "scissors":
+        case playerSelection === "scissors" && computerSelection === "rock":
+            losses++;
+            break;
+        case playerSelection === "rock" && computerSelection === "scissors":
+        case playerSelection === "paper" && computerSelection === "rock":
+        case playerSelection === "scissors" && computerSelection === "paper":
+            wins++;
+    }
+}
+
 function updateCounters() {
     document.querySelector(".wins").textContent = wins;
     document.querySelector(".losses").textContent = losses;
     document.querySelector(".draws").textContent = draws;
 }
 
-function evaluate(playerSelection, computerSelection) {
-    switch (true) {
-        case playerSelection === computerSelection:
-            draws++;
-            console.log("You play " + playerSelection + ", computer plays " + computerSelection + ". Draw");
-            break;
-        case playerSelection === "rock" && computerSelection === "paper":
-        case playerSelection === "paper" && computerSelection === "scissors":
-        case playerSelection === "scissors" && computerSelection === "rock":
-            losses++;
-            console.log("You play " + playerSelection + ", computer plays " + computerSelection + ". Computer wins");
-            break;
-        case playerSelection === "rock" && computerSelection === "scissors":
-        case playerSelection === "paper" && computerSelection === "rock":
-        case playerSelection === "scissors" && computerSelection === "paper":
-            wins++;
-            console.log("You play " + playerSelection + ", computer plays " + computerSelection + ". You win");
-    }
-}
+const gameResultContainer = document.querySelector(".game-result-container");
+const gameResults = document.createElement("div");
+gameResults.classList.add("game-result");
+
+const resetButtonContainer = document.querySelector(".reset-button-container");
+const resetButton = document.createElement("button");
+resetButton.classList.add("reset-button");
+resetButton.textContent = "Reset Game";
+resetButton.addEventListener("click", resetGame)
 
 function endGame() {
-    console.log(getGameResult() + " Wins: " + wins + " Losses: " + losses + " Draws: " + draws);
+    gameOver = true;
+    gameResults.textContent = getGameResult();
+    gameResultContainer.appendChild(gameResults);
+    resetButtonContainer.appendChild(resetButton);
+}
+
+function resetGame() {
     wins = 0;
     losses = 0;
     draws = 0;
+    updateCounters();
+    gameResultContainer.removeChild(gameResults);
+    resetButtonContainer.removeChild(resetButton);
+    gameOver = false;
 }
 
 function getGameResult() {
@@ -61,5 +81,3 @@ function getGameResult() {
         case wins === losses: return "Draw!";
     }
 }
-
-buttons.forEach((button => button.addEventListener("click", playRound)));

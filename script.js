e1d749d9c0f209e3deb27b7e78cec78bfb4885db
Chaostastic <roodey72mc@gmail.com>
2022-11-18 07:59:12 +0200

    Clean code

--- script.js
+++ script.js
@@ -1,83 +1,78 @@
-let gameOver = false;
-let wins = 0;
-let losses = 0;
-let draws = 0;
+let gameOver = false
+let wins = 0
+let losses = 0
+let draws = 0
 
-const buttons = document.querySelectorAll(".choice-button");
-buttons.forEach((button => button.addEventListener("click", playRound)));
+const CHOICES = ["rock", "paper", "scissors"]
 
-function playRound(e) {
-    if (gameOver) return;
-    const playerSelection = this.id;
-    evaluate(playerSelection, getComputerChoice());
-    updateCounters();
+function playRound(event) {
+    if (gameOver) return
+    const playerSelection = event.target.id
+    const computerSelection = CHOICES[(Math.floor(Math.random() * 3))]
+    evaluate(playerSelection, computerSelection)
+    updateCounters()
     if (wins >= 5 || losses >= 5) {
-        endGame();
-    }
-}
-
-function getComputerChoice() {
-    switch (Math.floor(Math.random() * 3)) {
-        case 0: return "rock";
-        case 1: return "paper";
-        case 2: return "scissors";
+        endGame()
     }
 }
 
 function evaluate(playerSelection, computerSelection) {
     switch (true) {
         case playerSelection === computerSelection:
-            draws++;
-            break;
+            draws++
+            break
         case playerSelection === "rock" && computerSelection === "paper":
         case playerSelection === "paper" && computerSelection === "scissors":
         case playerSelection === "scissors" && computerSelection === "rock":
-            losses++;
-            break;
+            losses++
+            break
         case playerSelection === "rock" && computerSelection === "scissors":
         case playerSelection === "paper" && computerSelection === "rock":
         case playerSelection === "scissors" && computerSelection === "paper":
-            wins++;
+            wins++
     }
 }
 
 function updateCounters() {
-    document.querySelector(".wins").textContent = wins;
-    document.querySelector(".losses").textContent = losses;
-    document.querySelector(".draws").textContent = draws;
+    document.querySelector(".wins").textContent = wins
+    document.querySelector(".losses").textContent = losses
+    document.querySelector(".draws").textContent = draws
 }
 
-const gameResultContainer = document.querySelector(".game-result-container");
-const gameResults = document.createElement("div");
-gameResults.classList.add("game-result");
-
-const resetButtonContainer = document.querySelector(".reset-button-container");
-const resetButton = document.createElement("button");
-resetButton.classList.add("reset-button");
-resetButton.textContent = "Reset Game";
-resetButton.addEventListener("click", resetGame)
-
 function endGame() {
-    gameOver = true;
-    [gameResults.textContent, gameResults.style.color] = getGameResult();
-    gameResultContainer.appendChild(gameResults);
-    resetButtonContainer.appendChild(resetButton);
+    gameOver = true
+    [gameResults.textContent, gameResults.style.color] = getGameResult()
+    gameResultContainer.appendChild(gameResults)
+    resetButtonContainer.appendChild(resetButton)
 }
 
 function resetGame() {
-    wins = 0;
-    losses = 0;
-    draws = 0;
-    updateCounters();
-    gameResultContainer.removeChild(gameResults);
-    resetButtonContainer.removeChild(resetButton);
-    gameOver = false;
+    wins = 0
+    losses = 0
+    draws = 0
+    updateCounters()
+    gameResultContainer.removeChild(gameResults)
+    resetButtonContainer.removeChild(resetButton)
+    gameOver = false
 }
 
 function getGameResult() {
     switch (true) {
-        case wins > losses: return ["You win!", "green"];
-        case wins < losses: return ["Computer wins!", "red"];
-        // case wins === losses: return ["Draw!", "orange"];
+        case wins > losses: return ["You win!", "green"]
+        case wins < losses: return ["Computer wins!", "red"]
+        // case wins === losses: return ["Draw!", "orange"]
     }
 }
+
+const buttons = document.querySelectorAll(".choice-button")
+buttons.forEach((button => button.addEventListener("click", playRound)))
+
+const gameResultContainer = document.querySelector(".game-result-container")
+const gameResults = document.createElement("div")
+gameResults.classList.add("game-result")
+
+const resetButtonContainer = document.querySelector(".reset-button-container")
+const resetButton = document.createElement("button")
+resetButton.classList.add("reset-button")
+resetButton.textContent = "Reset Game"
+resetButton.addEventListener("click", resetGame)
